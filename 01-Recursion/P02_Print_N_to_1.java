4936225f74224ae0d98c984c034f21dab61a00dc
Ritesh-Koomar <139606742+Ritesh-Koomar@users.noreply.github.com>
2024-08-31 18:49:15 +0530

    Create P02_Print_N_to_1.java

---
+++ 01-Recursion/P02_Print_N_to_1.java
@@ -0,0 +1,14 @@
+public class Hello
+  {
+    public static void main(String[] args)
+    {
+         print(5); 
+    }
+    public static void print(int n)
+    {
+        if(n==0)
+          return ;
+        System.out.print(n);
+        fun(n-1);
+    }
+  }
